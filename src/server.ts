6467aed718a4b18357406237d029ed63d3cd4809
Craig Fay <craigfay1@gmail.com>
2019-08-10 14:25:06 -0500

    Convert Middleware into a class

--- src/server.ts
+++ src/server.ts
@@ -105,25 +105,25 @@ function listen(port:number=0) {
  */
 function responseShorthand(response:any):HttpResponse {
   if (typeof response == 'string') {
-    return Object.freeze({
+    return {
       status: 200,
       headers: { 'content-type': 'text/html' },
       body: response 
-    })
+    }
   }
   else if (typeof response == 'number' && statusMessages[response]) {
-    return Object.freeze({
+    return {
       status: response,
       headers: { 'content-type': 'text/plain' },
       body: statusMessages[response]
-    })
+    }
   }
   else if (typeof response == 'object' && !(response instanceof HttpResponse)) {
-    return Object.freeze({
+    return {
       status: 200,
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(response),
-    })
+    }
   }
   else return response;
 }
@@ -156,19 +156,23 @@ export function server() {
   return new Server();
 }
 
-function middleware(method, urlpattern, handler) {
-  return {
-    method,
-    urlpattern,
-    handler,
-    match: (url) => new UrlPattern(urlpattern).match(url),
+class Middleware {
+  method: 'string';
+  urlpattern: 'string';
+  handler: (req, meta) => any;
+  match: (url) => { [key:string]: string };
+  constructor(method, urlpattern, handler) {
+    this.method = method;
+    this.urlpattern = urlpattern;
+    this.handler = handler;
+    this.match = url => new UrlPattern(urlpattern).match(url);
   }
 }
 
 function route(method, urlpattern, handler) {
   method = method.toUpperCase();
   if (!methods[method]) throw new Error(`Unsupported verb "${method}".`);
-  this.middlewares[method].push(middleware(method, urlpattern, handler));
+  this.middlewares[method].push(new Middleware(method, urlpattern, handler));
 }
 
 function methodStacks() {
