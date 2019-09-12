e580c422ce884a609763102d4981387271d55fb2
Craig Fay <craigfay1@gmail.com>
2019-09-11 22:43:56 -0500

    Cleanup

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,4 @@
 import { includeTests, run } from './runner'
-import {strict as assert } from 'assert'
-
-function exampleTest() {
-  assert.equal(true, false)
-}
 
 const testSuite = includeTests(
   require('./http.test'),
